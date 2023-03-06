032fc77c3f5b444939618620f055dbdbeabcdbc6
Stefan Pejcic <pejcha1994@gmail.com>
2023-03-06 14:59:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,20 @@ mv src/hostname.php /usr/local/cpanel/base/frontend/paper_lantern/loginlog/src
 mv assets/css/main.css /usr/local/cpanel/base/frontend/paper_lantern/loginlog/assets/css
 
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/loginlog/loginlog.tar.gz --theme paper_lantern
+
+
+
+
+mkdir -p /usr/local/cpanel/base/frontend/jupiter/loginlog
+mkdir -p /usr/local/cpanel/base/frontend/jupiter/loginlog/src
+mkdir -p /usr/local/cpanel/base/frontend/jupiter/loginlog/assets
+mkdir -p /usr/local/cpanel/base/frontend/jupiter/loginlog/assets/css
+
+
+mv loginlog.tar.gz /usr/local/cpanel/base/frontend/jupiter/loginlog
+mv lastlogin.live.php /usr/local/cpanel/base/frontend/jupiter/loginlog
+mv src/Account.php /usr/local/cpanel/base/frontend/jupiter/loginlog/src
+mv src/hostname.php /usr/local/cpanel/base/frontend/jupiter/loginlog/src
+mv assets/css/main.css /usr/local/cpanel/base/frontend/jupiter/loginlog/assets/css
+
+/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/jupiter/loginlog/loginlog.tar.gz --theme jupiter
