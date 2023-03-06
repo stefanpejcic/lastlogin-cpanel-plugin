a3fd9cf83912ea14ebca380398f7c790c7c99678
Stefan Pejcic <pejcha1994@gmail.com>
2023-03-06 15:01:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,13 @@ mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/loginlog/assets
 mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/loginlog/assets/css
 
 
-mv loginlog.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/loginlog
-mv lastlogin.live.php /usr/local/cpanel/base/frontend/paper_lantern/loginlog
-mv src/Account.php /usr/local/cpanel/base/frontend/paper_lantern/loginlog/src
-mv src/hostname.php /usr/local/cpanel/base/frontend/paper_lantern/loginlog/src
-mv assets/css/main.css /usr/local/cpanel/base/frontend/paper_lantern/loginlog/assets/css
+cp loginlog.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/loginlog
+cp lastlogin.live.php /usr/local/cpanel/base/frontend/paper_lantern/loginlog/lastlogin.live.php
+cp src/Account.php /usr/local/cpanel/base/frontend/paper_lantern/loginlog/src/Account.php
+cp src/hostname.php /usr/local/cpanel/base/frontend/paper_lantern/loginlog/src/hostname.php
+cp assets/css/main.css /usr/local/cpanel/base/frontend/paper_lantern/loginlog/assets/css/main.css
 
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/loginlog/loginlog.tar.gz --theme paper_lantern
-
 
 
 
